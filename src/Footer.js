d0d90d3dad17fc12c8fbe8295ee74e2a34078cc4
Bucklan <as1kenjebaev@gmail.com>
2023-09-24 23:17:46 +0600

    Update project structure and README
    Simplified README to avoid overwhelming visitors. Removed bootstrapping and script details, they are now documented elsewhere. Added new layout files (Header.js, Footer.js, Technologies.js) and modified existing files(App.js, App.css) to implement a new design and component structure for more organized code base.

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <div>
+            THIS IS FOOTER
+            <br/>
+            <a href="">HOME</a> -
+            <a href="">ABOUT US</a> -
+             <a href="">WORKS</a>
+        </div>
+    );
+}
+export default Footer;
---
+++ src/Technologies.js
@@ -0,0 +1,15 @@
+function Technologies(){
+    return (
+        <div>
+      <ul>
+          THIS IS TECHNOLOGIES
+          <br/>
+          <li>css</li>
+          <li>js</li>
+          <li>html</li>
+          <li>react</li>
+      </ul>
+        </div>
+    );
+}
+export default Technologies;
