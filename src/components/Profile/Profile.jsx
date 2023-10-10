bf27cda1b8b972bfa8f5bf2615d5edfe06217348
Bucklan <as1kenjebaev@gmail.com>
2023-10-10 19:52:19 +0600

    "Refactor components structure for better organization"
    Refactored the project's file structure by organizing components into individual directories. This included moving related CSS module files into corresponding component directories and renaming them for clarity. Addition of Dialog component is also reflected. This improves project navigation and consistency across components. This change doesn't affect any functionality.

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import classes from'./Profile.module.css'
+import classes from './Profile.module.css'
 
 function Profile() {
     return <div className={classes.content}>
