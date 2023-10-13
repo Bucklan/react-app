ca1f24cfd984cd4f861d026272fa0c8239c640ea
Bucklan <as1kenjebaev@gmail.com>
2023-10-14 02:14:56 +0600

    Update styling and functionality of Navbar and Dialogs
    Added padding and removed text-decoration for Navbar items to improve visual appearance. Enhanced active link styling in Navbar for user clarity. Updated Dialogs component to use grid layout for structured visual layout and added active class in dialog items for better user interface. Substituted a tags with NavLink in Navbar.jsx for routing.

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,32 @@
 import React from "react";
-import success from "./Dialogs.module.css";
-const Dialogs = (props) => {
+import classes from "./Dialogs.module.css";
+
+const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogItems}>
+
+                <div className={classes.dialog+ ' ' + classes.active}>
+                    FNdjskfd
+                </div>
+                <div className={classes.dialog+ ' ' + classes.active}>
+                    fjdsfds
+                </div>
+                <div className={classes.dialog+ ' ' + classes.active}>
+                    fjkdshfds
+                </div>
+                <div className={classes.dialog+ ' ' + classes.active}>
+                    fjdshkfjd
+                </div>
+                <div className={classes.dialog+ ' ' + classes.active}>
+                    qweqwewq ewqew
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>how are you</div>
+                <div className={classes.message}>i'm fine,thanks!and you</div>
+            </div>
         </div>
     )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import classes from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 function Navbar() {
     return <div className={classes.nav}>
         <div className={classes.item}>
-            <a href="/profile">Profile</a><br/>
-            <a href="/dialogs">Dialogs</a><br/>
+            <NavLink to="/profile" activeClassName={classes.activeLink}>Profile</NavLink><br/>
+            <NavLink to="/dialogs" activeClassName={classes.activeLink}>Dialogs</NavLink><br/>
             <a href="#">News</a><br/>
             <a href="#">Music</a><br/>
             <a href="#">Settings</a>
